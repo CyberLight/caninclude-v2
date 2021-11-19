633aa4e727d530526a78d271210359d13e0d4791
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-19 07:49:40 +0000

    Changed layout of Main component with sections

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -1,9 +1,19 @@
 import { h } from 'preact';
 import Search from '../../components/search'
+import Logo from '../../components/logo'
 
 const Main = () => (
-	<main class="flex justify-center">
-		<Search />
+	<main class="flex flex-col items-center justify-center">
+		<h1 class="sr-only">Can I include</h1>
+		<section class="flex flex-col items-center">
+            <h2 class="sr-only">Welcome</h2>
+			<Logo />
+            <span class="text-xs font-thin text-center">Based on HTML5 <br /> specification</span>
+        </section>
+		<section class="flex flex-col items-center">
+			<h2 class="sr-only">Can I include a child tag to a parent tag?</h2>
+			<Search />
+		</section>
 	</main>
 );
 
