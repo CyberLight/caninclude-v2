3cfea04255a7e8a426f99aaa5df9839c5adf4563
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-19 07:40:53 +0000

    Cleanup package.json and preact.config from unneeded plugin

--- preact.config.js
+++ preact.config.js
@@ -1,12 +1,4 @@
-module.exports = (config, env, helpers, params = defaultParams) => {
-  const purgecss = require('@fullhuman/postcss-purgecss')({
-    // Specify the paths to all of the template files in your project
-    content: ['./src/**/*.js'],
-
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(params.regex) || [],
-  });
-
+module.exports = (config, env, helpers) => {
   const postCssLoaders = helpers.getLoadersByName(config, 'postcss-loader');
   postCssLoaders.forEach(({ loader }) => {
     if (!loader.options.postcssOptions.plugins) {
@@ -16,11 +8,6 @@ module.exports = (config, env, helpers, params = defaultParams) => {
     
     // Add tailwind css at the top.
     plugins.unshift(require('tailwindcss'));
-
-    // Add PurgeCSS only in production.
-    if (env.production) {
-      plugins.push(purgecss);
-    }
   });
   return config;
 };
