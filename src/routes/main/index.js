81f7746e9c2331f725cf0ddbc32b49bd8d6c500c
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-19 17:40:55 +0000

    Fixed style for texts under Logo

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -3,12 +3,12 @@ import Search from '../../components/search'
 import Logo from '../../components/logo'
 
 const Main = () => (
-	<main class="flex flex-col items-center justify-center">
+	<main class="flex flex-col items-center justify-center flex-grow">
 		<h1 class="sr-only">Can I include</h1>
 		<section class="flex flex-col items-center">
             <h2 class="sr-only">Welcome</h2>
 			<Logo />
-            <span class="text-xs font-thin text-center">Based on HTML5 <br /> specification</span>
+            <span class="text-xs text-center">Based on HTML spec | Last Updated DD Mmmmmm YYYY</span>
         </section>
 		<section class="flex flex-col items-center">
 			<h2 class="sr-only">Can I include a child tag to a parent tag?</h2>
