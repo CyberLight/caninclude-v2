37e33426d5fe6f8f1011560e911fdc020f1a162f
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-19 07:47:08 +0000

    Added code for calculations of hide/show flag of mini logo

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,40 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
+import { useState } from 'preact/hooks';
 import Header from './header';
 
 // Code-splitting is automated for `routes` directory
 import Main from '../routes/main';
-import Profile from '../routes/profile';
+import Result from '../routes/result';
 
-const App = () => (
-	<div id="app">
-		<Header />
-		<Router>
-			<Main path="/" />
-			<Profile path="/profile/" user="me" />
-		</Router>
-	</div>
-)
+const App = () => {
+	const [showLogo, setShowLogo] = useState(false);
+
+	const onRouteChanged = (e) => {
+		switch (e.url) {
+			case '/result/': {
+				setShowLogo(true);
+				break;
+			}
+			case '/about': {
+				setShowLogo(true);
+				break;
+			}
+			default: {
+				setShowLogo(false);
+				break;
+			}
+		}
+	}
+	return (
+		<div id="app">
+			<Header showLogo={showLogo} />
+			<Router onChange={onRouteChanged}>
+				<Main path="/" />
+				<Result path="/caninclude/" user="me" />
+			</Router>
+		</div>
+	);
+}
 
 export default App;
