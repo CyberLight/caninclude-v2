382fb613869581e8946b80697f532d2103a003c2
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-19 17:37:54 +0000

    Added custom min-h-14 for header menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       },
       boxShadow: {
         'inset-thin': 'inset 0 2px 5px rgba(0,0,0,.2)'
+      },
+      minHeight: {
+        'h-14': 'height: 3.5rem/* 56px */'
       }
     }
   },
