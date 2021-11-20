a033e037f4ef6af118c8b08a29fca27b28e678e3
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-20 06:46:00 +0000

    Added usage of Hamburger inside Header + additional styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,40 +1,48 @@
 import { h } from 'preact';
 import Match, { Link } from 'preact-router/match';
+import { useState } from 'preact/hooks';
+import Hamburger from '../hamburger';
 
 const Header = () => {
+	const [menuClosed, setMenuClosed] = useState(true);
+	const onMenuBtnClick = (e) => setMenuClosed(!e.target.checked);
+	const onMenuItemClick = () => setMenuClosed(true);
+
 	return (
 		<header className="flex flex-col sm:flex-row bg-purple-600 text-gray-50 min-h-14 items-center justify-center">
-			<nav class="flex items-center w-full sm:w-auto">
-				<ul class="flex flex-col text-center w-full sm:flex-row sm:w-auto flex-wrap">
-					<li>
+			<nav class={`flex flex-col text-center w-full sm:flex-row sm:w-auto relative ${ menuClosed && 'h-14 overflow-hidden'}`}>
+				<ul class="contents">
+					<li class="contents">
 						<Link 
-							activeClassName="bg-purple-400 bg-opacity-75 bg-no-repeat bg-full-h-3 bg-top bg-gradient-to-r from-purple-400 to-purple-400" 
+							activeClassName="bg-purple-400 bg-opacity-75 bg-no-repeat bg-full-h-3 bg-top bg-gradient-to-r from-purple-400 to-purple-400 order-first sm:order-none" 
 							class="block px-4 py-2 h-14 leading-10 hover:bg-purple-300 hover:bg-opacity-25 capitalize" 
-							href="/">
-								<div class="flex uppercase gap-2 items-center">
+							href="/"
+							onClick={onMenuItemClick}
+							>
+								<div class="flex uppercase gap-2 items-center justify-center">
 									<Match path="/">{({ matches }) => !matches && <span class="block w-9 h-9 bg-logo" />}</Match>
 									caninclude
 								</div>
-							</Link>
+						</Link>
 					</li>
-					<li>
+					<li class="contents">
 						<Link 
-							activeClassName="bg-purple-400 bg-opacity-75 bg-no-repeat bg-full-h-3 bg-top bg-gradient-to-r from-purple-400 to-purple-400" 
+							activeClassName="bg-purple-400 bg-opacity-75 bg-no-repeat bg-full-h-3 bg-top bg-gradient-to-r from-purple-400 to-purple-400 order-first sm:order-none" 
 							class="block px-4 py-2 h-14 leading-10 hover:bg-purple-300 hover:bg-opacity-25 capitalize" 
-							href="/about">about</Link>
+							href="/about"
+							onClick={onMenuItemClick}>about</Link>
 					</li>
-					<li>
+					<li class="contents">
 						<a 
 							target="_blank" 
 							rel="noreferrer" 
 							class="block px-2 py-2 h-14 leading-10 hover:bg-purple-300 hover:bg-opacity-25" 
-							href="http://github.com/CyberLight/caninclude-v2">Github</a>
+							href="https://github.com/CyberLight/caninclude-v2" onClick={onMenuItemClick}>Github</a>
 					</li>
 				</ul>
+				<Hamburger onClick={onMenuBtnClick} closed={menuClosed} />
 			</nav>
-			<ul class="flex flex-wrap font-thin justify-self-end">
-				<li class="px-4 py-2">v 2.0</li>
-			</ul>
+			<div class={`flex flex-wrap font-thin justify-self-end px-4 py-2 ${ menuClosed && 'hidden sm:block' }`}>v 2.0</div>
 		</header>
 	);
 };
