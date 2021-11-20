1a6813e2bd5bedc133f7310445c94f44822abb62
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-20 07:43:37 +0000

    Removed shown of loading component for Logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,17 +1,9 @@
 import { h } from 'preact';
-import { useState } from 'preact/hooks';
-
-const LogoLoading = () => (
-    <div class="animate-pulse absolute left-0 right-0 w-full h-full dark:bg-purple-400 bg-purple-600 rounded-lg text-current" />
-)
 
 const Logo = () => {
-    const [loading, setLoading] = useState(true);
-    const onImageLoaded = () => setLoading(false);
 
     return (
         <div class="relative w-28 h-28 mb-5">
-            { loading && <LogoLoading /> }
             <picture>
                 <source srcset="/assets/logo.svg" type="image/svg+xml" />
                 <source srcset="/assets/logo@1x.png, /assets/logo@2x.png 2x, /assets/logo@3x.png 3x" type="image/png" />
@@ -21,7 +13,6 @@ const Logo = () => {
                     class="object-contain h-full w-full text-current" 
                     alt="caninclude logo" 
                     src="/assets/logo@1x.png" 
-                    onLoad={onImageLoaded} 
                 />
             </picture> 
         </div>
