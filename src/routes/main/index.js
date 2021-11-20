a4662f6d8e2590f260d4d381d1ed960e07b359e8
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-20 11:59:57 +0000

    Added text variants for different screens under logo

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -8,7 +8,8 @@ const Main = () => (
 		<section class="flex flex-col items-center">
             <h2 class="sr-only">Welcome</h2>
 			<Logo />
-            <span class="text-xs text-center">Based on HTML spec | Last Updated DD Mmmmmm YYYY</span>
+            <span class="text-xs hidden sm:block text-center">Based on HTML spec | Last Updated DD Mmmmmm YYYY</span>
+            <span class="text-xs sm:hidden text-center">Last Updated DD Mmmmmm YYYY</span>
         </section>
 		<section class="flex flex-col items-center">
 			<h2 class="sr-only">Can I include a child tag to a parent tag?</h2>
