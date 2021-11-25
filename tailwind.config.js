d89038304afe4f0c67d5c0fa8ee71eacacbb95e3
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-25 04:33:52 +0000

    Added variants for traverse peer:checked inside sibling container + min-h-content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './src/template.html'],
@@ -15,12 +17,25 @@ module.exports = {
         'inset-thin': 'inset 0 2px 5px rgba(0,0,0,.2)'
       },
       minHeight: {
-        'h-14': 'height: 3.5rem/* 56px */'
+        content: 'calc(100vh - 3.5rem - 3.5rem)'
       }
     }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant, e }) => {
+      addVariant('peer-checked-fch', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.peer:checked ~ .${e(`peer-checked-fch${separator}${className}`)} > :first-child`
+        })
+      }),
+      addVariant('peer-not-checked-lch', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.peer:not(:checked) ~ .${e(`peer-not-checked-lch${separator}${className}`)} > :last-child`
+        })
+      })
+    })
+  ],
 }
