36d53609b1817cc466217738eb5ba71477aa546f
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-25 15:59:30 +0000

    Added usage of caninclude-analyzer inside api

--- server/api.js
+++ server/api.js
@@ -1,6 +1,14 @@
 const polka = require('polka');
 const spec = require('./spec.json');
 const params = require('./params.json');
+const {CanincludeAnalyzer, rules} = require('caninclude-analyzer');
+const analyzer = new CanincludeAnalyzer(rules);
+
+const resultsMap = {
+	true: 'can',
+	false: 'cant',
+	unknown: 'doubt'
+}
 
 module.exports = polka()
 	.get('/caninclude', (req, res) => {
@@ -12,5 +20,6 @@ module.exports = polka()
 		const result = { child: filteredTags[child], parent: filteredTags[parent] };
 		result.child.params = params[child].params;
 		result.parent.params = params[parent].params;
+		result.can = resultsMap[analyzer.canInclude({ name: child }, { name: parent })];
 		res.end(JSON.stringify({ ok: true, result }));
 	});
