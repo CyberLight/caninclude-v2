58e1fbdfdf866b94a98d0350079b2389a5e41fd2
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-25 04:35:38 +0000

    Changed style for footer for putting it always to the bottom of a page

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,14 @@
 import { h } from 'preact';
 
 const Footer = () => (
-    <footer class="flex flex-row font-thin h-20 bg-black justify-evenly items-center text-gray-200">    
-        <section class="py-1 px-2">
+    <footer class="flex flex-row absolute bottom-0 w-full font-thin h-14 bg-black justify-evenly items-center text-gray-200">    
+        <section class="p-2">
             <h3 class="text-sm">Links:</h3>
             <ul class="flex flex-row text-xs gap-2 flex-wrap">
                 <li><a href="https://github.com/CyberLight">Author - CyberLight</a></li>
             </ul>
         </section>
-        <section class="py-1 px-2 border-l border-gray-400 hidden sm:block">
+        <section class="p-2 border-l border-gray-400 hidden sm:block">
             <h3 class="text-sm">Special thanks:</h3>
             <ul class="flex flex-row text-xs gap-2 flex-wrap">
                 <li><a href="https://caniuse.com/">Can I Use</a></li>
