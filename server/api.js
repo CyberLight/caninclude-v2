64ca0a53c89a8263f9f47fb3b914dec68fe8e447
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-26 07:57:36 +0000

    Added mapping and filtering params for child and parent tags

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
 const polka = require('polka');
+const get = require('lodash/get');
 const spec = require('./spec.json');
 const params = require('./params.json');
 const {CanincludeAnalyzer, rules} = require('caninclude-analyzer');
@@ -20,6 +21,25 @@ module.exports = polka()
 		const result = { child: filteredTags[child], parent: filteredTags[parent] };
 		result.child.params = params[child].params;
 		result.parent.params = params[parent].params;
+		const childParamList = result.child.Categories
+			.flatMap(
+				(block) => block.filter(
+					(line) => typeof line !== 'string' && get(result, 'child.params.Categories', [])
+					.includes(line.hashText)
+				)
+			).reduce((distinct, line) => ({[line.hashText]: line, ...distinct}), {});
+		const parentParamList = result.parent.ContentModel
+			.flatMap(
+				(block) => block.filter(
+					(line) => typeof line !== 'string' && get(result, 'parent.params.ContentModel', [])
+					.includes(line.hashText)
+				)
+			).reduce((distinct, line) => ({[line.hashText]: line, ...distinct}), {});
+
+		result.child.params = Object.values(childParamList);
+		result.parent.params = Object.values(parentParamList);
+		console.warn('<<<<<< result.child.params:', result.child.params);
+		console.warn('<<<<<< result.parent.params: ', result.parent.params);
 		result.can = resultsMap[analyzer.canInclude({ name: child, params: childParams }, { name: parent, params: parentParams })];
 		res.end(JSON.stringify({ ok: true, result }));
 	});
