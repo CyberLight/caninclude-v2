c97f592ad9ea2872cea0aea9668d23360df5a0b1
Alexandr Vishniakov <cyberlight@live.ru>
2021-12-02 11:26:22 +0000

    Added shallow copying of objects for equal tags (in this case each two tags are the same object)

--- server/api.js
+++ server/api.js
@@ -1,4 +1,3 @@
-const polka = require('polka');
 const get = require('lodash/get');
 const spec = require('./spec.json');
 const params = require('./params.json');
@@ -20,8 +19,12 @@ function makeHeadersFor(jsonData) {
 	return headers;
 }
 
-module.exports = polka()
-	.get('/caninclude', (req, res) => {
+function copyObject(o) {
+	return {...o};
+}
+
+module.exports = {
+	caninclude: (req, res) => {
 		const { child, parent, childParams, parentParams } = req.query;
 		const requiredQueryParams = [child, parent];
 
@@ -34,38 +37,37 @@ module.exports = polka()
 		const filteredTags = spec
 			.filter((tag) => [child, parent].includes(tag.tag));
 			
-		if (filteredTags.length < 2) {
+		if (filteredTags.length < 2 && child !== parent) {
 			const data = JSON.stringify({ ok: false, message: 'Some values from parameters were not found',  type: 'warning' });
 			res.writeHead(400, makeHeadersFor(data));
 			return res.end(data);
 		}
 
 		const tags = filteredTags.reduce((o, tag) => ({[tag.tag]: tag, ...o}), {});	
-		const result = { child: tags[child], parent: tags[parent] };
-		result.child.params = params[child].params;
-		result.parent.params = params[parent].params;
+		const result = { child: copyObject(tags[child]), parent: copyObject(tags[parent]) };
+		result.child.params = copyObject(params[child].params);
+		result.parent.params = copyObject(params[parent].params);
+		const childParamsList = get(result, 'child.params.Categories', []);
+		const parentParamsList = get(result, 'parent.params.ContentModel', []);
+
+		const toObject = (distinct, line) => ({[line.hashText]: line, ...distinct});
+		const onlyMatchedHref = (paramsList) => (line) => typeof line !== 'string' && paramsList.includes(line.hashText);
+
 		const childParamList = result.child.Categories
-			.flatMap(
-				(block) => block.filter(
-					(line) => typeof line !== 'string' && get(result, 'child.params.Categories', [])
-					.includes(line.hashText)
-				)
-			).reduce((distinct, line) => ({[line.hashText]: line, ...distinct}), {});
+			.flatMap((block) => block.filter(onlyMatchedHref(childParamsList))).reduce(toObject, {});
+
 		const parentParamList = result.parent.ContentModel
-			.flatMap(
-				(block) => block.filter(
-					(line) => typeof line !== 'string' && get(result, 'parent.params.ContentModel', [])
-					.includes(line.hashText)
-				)
-			).reduce((distinct, line) => ({[line.hashText]: line, ...distinct}), {});
+			.flatMap((block) => block.filter(onlyMatchedHref(parentParamsList))).reduce(toObject, {});
 
 		result.child.params = Object.values(childParamList);
 		result.parent.params = Object.values(parentParamList);
+
 		result.include = analyzer.canInclude(
 			{ name: child, params: childParams }, 
 			{ name: parent, params: parentParams }, 
 			true
 		);
+
 		result.include.can = resultsMap[result.include.can];
 		if (result.include.alternative) {
 			result.include.alternative = {
@@ -76,4 +78,5 @@ module.exports = polka()
 		const data = JSON.stringify({ ok: true, result });
 		res.writeHead(200, makeHeadersFor(data));
 		res.end(data);
-	});
+	}
+}
