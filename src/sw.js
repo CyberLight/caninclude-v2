b68b150eb9f41c978c91d3d7c708f3bf6409b9d2
Alexandr Vishniakov <cyberlight@live.ru>
2021-12-03 12:36:49 +0000

    Added re-register of service worker and caching for api urls

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,21 @@
 import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+ if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations()
+        .then((registrations) => {
+            for(let registration of registrations) {
+                registration.unregister();
+            }
+        });
+}
+
+registerRoute(
+    /\/api\/.*/,
+    new StaleWhileRevalidate(),
+    'GET'
+);
 
 setupRouting();
 setupPrecaching(getFiles());
