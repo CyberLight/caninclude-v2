b6be3cbb66e87f4513dbd8beeffb9d39cf89e6a2
Alexandr Vishniakov <cyberlight@live.ru>
2021-12-03 09:58:49 +0000

    Refactoring: Moved to separate file code of DefaultSectionContent component

---
+++ src/components/default/sectioncontent/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+const DefaultSectionsContent = () => (
+	<>
+		<li class="w-1/2 my-2 h-3 list-none bg-gray-400 dark:bg-gray-600" />
+		<li class="w-1/3 my-2 h-3 list-none bg-gray-400 dark:bg-gray-600" />
+		<li class="w-1/2 my-2 h-3 list-none bg-gray-400 dark:bg-gray-600" />
+	</>
+);
+
+export default DefaultSectionsContent;
